d6a3e1f3567cc359cd4bcf4b624dc89bcca66b1d
Lucas Vinicius <110206119+lucasvinip@users.noreply.github.com>
2023-12-21 22:17:38 -0300

    Update database_register_alphacode.sql

--- database/database_register_alphacode.sql
+++ database/database_register_alphacode.sql
@@ -1,5 +1,3 @@
-show databases;
-
 create database register_alphacode;
 
 use register_alphacode;
@@ -13,50 +11,3 @@ create table signup_user (
     occupation  varchar(100),
     contact_cellphone varchar(25) not null
 );
-
-insert into signup_user (
-	 name,
-	 email,
-	 date_of_birth,
-	 contact_phone,
-	 occupation,
-	 contact_cellphone
- )
-values (
-		'Daniella Dias',
-		 'daniellapereira207@gmail.com',
-		 '1990-05-01',
-		 '',
-		 'TSB',
-		 '11975936195'
- );
-insert into signup_user (
-	 name,
-	 email,
-	 date_of_birth,
-	 contact_phone,
-	 occupation,
-	 contact_cellphone
- )
-values (
-		'Lucas Pereira',
-		 'l.vinicius1577@gmail.com',
-		 '2004-02-19',
-		 '',
-		 'Dev',
-		 '11987654321'
- );
-
-DROP TABLE signup_user;
-
-select * from signup_user where id = 5;
-
-select * from signup_user;
-
-delete from signup_user where id = 1;
-
-update signup_user
-set
-    occupation = 'ASB'
-WHERE
-    id = 3;
